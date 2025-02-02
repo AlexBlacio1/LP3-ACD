8c5871782f0642548575a9577fc32d1ac849f340
AlexBlacio1 <vallejoalexiis@gmail.com>
2025-02-02 03:25:14 -0500

    2d commit

--- src/main/java/com/clinic/app/clinic/entidad/Historial.java
+++ src/main/java/com/clinic/app/clinic/entidad/Historial.java
@@ -7,11 +7,10 @@ import lombok.Setter;
 
 import java.util.Date;
 
-@Data
+
 public class Historial {
 
-    @Setter
-    @Getter
+
     @Size(min = 10, max = 50, message = "El nombre debe tener entre 10 y 50 caracteres.")
     private String name;
 
@@ -73,4 +72,172 @@ public class Historial {
 
     @Size(max = 1000, message = "Las observaciones deben tener un máximo de 1000 caracteres.")
     private String observaciones;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public Date getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(Date fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public String getEstadoCivil() {
+        return estadoCivil;
+    }
+
+    public void setEstadoCivil(String estadoCivil) {
+        this.estadoCivil = estadoCivil;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNumeroContacto() {
+        return numeroContacto;
+    }
+
+    public void setNumeroContacto(String numeroContacto) {
+        this.numeroContacto = numeroContacto;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getAntecedentesFamiliares() {
+        return antecedentesFamiliares;
+    }
+
+    public void setAntecedentesFamiliares(String antecedentesFamiliares) {
+        this.antecedentesFamiliares = antecedentesFamiliares;
+    }
+
+    public String getAlergias() {
+        return alergias;
+    }
+
+    public void setAlergias(String alergias) {
+        this.alergias = alergias;
+    }
+
+    public String getMedicamentosActuales() {
+        return medicamentosActuales;
+    }
+
+    public void setMedicamentosActuales(String medicamentosActuales) {
+        this.medicamentosActuales = medicamentosActuales;
+    }
+
+    public String getEnfermedadesCronicas() {
+        return enfermedadesCronicas;
+    }
+
+    public void setEnfermedadesCronicas(String enfermedadesCronicas) {
+        this.enfermedadesCronicas = enfermedadesCronicas;
+    }
+
+    public String getCirugiasPrevias() {
+        return cirugiasPrevias;
+    }
+
+    public void setCirugiasPrevias(String cirugiasPrevias) {
+        this.cirugiasPrevias = cirugiasPrevias;
+    }
+
+    public String getMotivoConsulta() {
+        return motivoConsulta;
+    }
+
+    public void setMotivoConsulta(String motivoConsulta) {
+        this.motivoConsulta = motivoConsulta;
+    }
+
+    public String getDiagnostico() {
+        return diagnostico;
+    }
+
+    public void setDiagnostico(String diagnostico) {
+        this.diagnostico = diagnostico;
+    }
+
+    public String getTratamiento() {
+        return tratamiento;
+    }
+
+    public void setTratamiento(String tratamiento) {
+        this.tratamiento = tratamiento;
+    }
+
+    public Date getFechaInicioTratamiento() {
+        return fechaInicioTratamiento;
+    }
+
+    public void setFechaInicioTratamiento(Date fechaInicioTratamiento) {
+        this.fechaInicioTratamiento = fechaInicioTratamiento;
+    }
+
+    public String getReferenciaMedica() {
+        return referenciaMedica;
+    }
+
+    public void setReferenciaMedica(String referenciaMedica) {
+        this.referenciaMedica = referenciaMedica;
+    }
+
+    public String getUltimaConsulta() {
+        return ultimaConsulta;
+    }
+
+    public void setUltimaConsulta(String ultimaConsulta) {
+        this.ultimaConsulta = ultimaConsulta;
+    }
+
+    public String getObservaciones() {
+        return observaciones;
+    }
+
+    public void setObservaciones(String observaciones) {
+        this.observaciones = observaciones;
+    }
 }
